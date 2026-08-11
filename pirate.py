8e43c954dc9eea731cba4871276d3b7287ee50c8
Y0U4R34P1R4T3 <rakoflar@gmail.com>
2026-08-11 12:44:49 -0300

    Update pirate.py

--- pirate.py
+++ pirate.py
@@ -450,7 +450,7 @@ def main():
         print("  pirate install <juego>      -> Descarga e instala un elemento")
         print("  pirate upgrade              -> Actualiza los juegos instalados")
         print("  pirate request \"<juego>\"   -> Solicita que agreguen contenido")
-        print("\n Este repositorio consigue los juegos de distintas paginas como: SteamRIP, FitGirl Repacks y ElEnemigos. Para las roms paginas como Romsfun")
+        print("\n Este repositorio agradece su uso del repositorio")
         sys.exit(1)
 
     cmd = sys.argv[1].lower()
